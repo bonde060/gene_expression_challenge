db0a33d7830cdd516609ca979f4fa7ae5646bf0c
Annie Bonde <bonde060@umn.edu>
2023-04-28 13:09:34 -0500

    Random search CV optimization

--- Main.py
+++ Main.py
@@ -2,35 +2,87 @@ import numpy as np
 import pandas as pd
 import sklearn
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.model_selection import cross_validate, KFold
-from sklearn.model_selection import RepeatedStratifiedKFold
+from sklearn.model_selection import RandomizedSearchCV, GridSearchCV
 from sklearn.metrics import mean_squared_error
 
+def evaluate(model, test_features, test_labels):
+    predictions = model.predict(test_features)
+    errors = abs(predictions - test_labels)
+    mape = 100 * np.mean(errors / test_labels)
+    accuracy = 100 - mape
+    print('Model Performance')
+    print('Average Error: {:0.4f}'.format(np.mean(errors)))
+    print('Accuracy = {:0.2f}%.'.format(accuracy))
+
+    return accuracy
+
+#################### Read and format data ########################
 data = pd.read_table("Challenge_GIN_release_profile_17804library_181query.txt", delimiter="\t", header=0, index_col=0)
-
-model = RandomForestRegressor(max_depth=5, max_features=0.2, bootstrap=True, random_state=0, n_jobs=-2)
-
 # take rows that are present in the columns
 X = data.loc[data.columns]
 print(X)
-
 # transpose is the output
 Y = data.T
 print(Y.shape)
 
-#fit model
-model.fit(X, Y)
+#################### Random grid search to find decent parameters #################
+# Number of trees in random forest
+n_estimators = [int(x) for x in np.linspace(start = 200, stop = 2000, num = 10)]
+# Number of features to consider at every split
+max_features = [0.1, 0.2, 0.3]
+# Maximum number of levels in tree
+max_depth = [int(x) for x in np.linspace(10, 110, num = 11)]
+max_depth.append(None)
+# Minimum number of samples required to split a node
+min_samples_split = [2, 5, 10]
+# Minimum number of samples required at each leaf node
+min_samples_leaf = [1, 2, 4]
+# Method of selecting samples for training each tree
+bootstrap = [True, False]
+# Create the random grid
+random_grid = {'n_estimators': n_estimators,
+               'max_features': max_features,
+               'max_depth': max_depth,
+               'min_samples_split': min_samples_split,
+               'min_samples_leaf': min_samples_leaf,
+               'bootstrap': bootstrap}
+print(random_grid)
 
+# Use the random grid to search for best hyperparameters
+# First create the base model to tune
+rf = RandomForestRegressor()
+# Random search of parameters, using 3 fold cross validation,
+# search across 100 different combinations, and use all available cores
+rf_random = RandomizedSearchCV(estimator = rf, param_distributions = random_grid, n_iter = 100, cv = 3, verbose=2, random_state=42, n_jobs = -1)
+# Fit the random search model
+rf_random.fit(X, Y)
+print(rf_random.best_params_)
 
-print(f"Original Y interaction for gene {data.columns[0]}:\n{data.loc[:, data.columns[0]]}")
-#result = pd.DataFrame(model.predict([X.iloc[0]]))
-#print(f"Predicted values: \n{result}")
-#print(result.shape)
+base_model = RandomForestRegressor(n_estimators = 10, random_state = 42)
+base_model.fit(X, Y)
+base_accuracy = evaluate(base_model, X, Y)
+best_random = rf_random.best_estimator_
+random_accuracy = evaluate(best_random, X, Y)
+print('Improvement of {:0.2f}%.'.format( 100 * (random_accuracy - base_accuracy) / base_accuracy))
 
-#evaluate
-y_pred = model.predict(X.iloc[:10, :])
-mse = mean_squared_error(Y.iloc[:10, :], y_pred)
-print("Mean Squared Error: ", mse)
+'''
+################# Grid search with cross val #############
+# Change this parameter grid based on the results of random search
+param_grid = {
+    'bootstrap': [True],
+    'max_depth': [80, 90, 100, 110],
+    'max_features': [2, 3],
+    'min_samples_leaf': [3, 4, 5],
+    'min_samples_split': [8, 10, 12],
+    'n_estimators': [100, 200, 300, 1000]
+}
+# Create a based model
+rf = RandomForestRegressor()
+# Instantiate the grid search model
+grid_search = GridSearchCV(estimator = rf, param_grid = param_grid,
+                          cv = 3, n_jobs = -1, verbose = 2)
+'''
+
 
 
 
